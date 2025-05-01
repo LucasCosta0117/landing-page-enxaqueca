7257964a00d77ccdba3dc7fcab985e2d7687ce0c
Lucas Costa <lucas.costa@simova.com.br>
2025-04-30 22:41:39 -0300

    refactor: redesenhado componente do botão de venda do ebook

--- src/components/SallButton.vue
+++ src/components/SallButton.vue
@@ -1,7 +1,12 @@
 <template>
-    <a :href="seleUrl">
-        <input type="button" class="btn-input" :value="btnTextValue" />
-    </a>
+    <div class="sall-btn-container">
+        <a :href="seleUrl">
+            <input type="button" class="btn-input" :value="btnTextValue" />
+        </a>
+        <a :href="seleUrl">
+            <font-awesome-icon icon="fa-solid fa-download" bounce />
+        </a>
+    </div>
 </template>
 
 <script>
@@ -45,6 +50,12 @@ export default {
 </script>
 
 <style scoped>
+.sall-btn-container {
+    display: flex;
+    gap: 1rem;
+    justify-content: start;
+    align-items: center;
+}
 .btn-input {
     background: v-bind(btnBgColor);
     color: v-bind(btnTextColor);
@@ -62,11 +73,45 @@ export default {
     box-shadow: none;
     cursor: pointer;
 }
+.fa-bounce {
+    /* display: none; */
+    font-size: 2rem;
+    color: v-bind(btnBgColor);
+  animation-duration: 2.5s;
+}
 
 /** Responsividade para telas até 768px (Tabletes) */
 @media(min-width: 481px) and (max-width: 768px) {
   .btn-input {
     font-size: 1.25rem;
+  }
+}
+
+/** Responsividade para telas até 1024px (Notebooks/telas HD) */
+@media(min-width: 769px) and (max-width: 1024px) {
+  .btn-input {
+    font-size: 1.25rem;
+  }
+}
+
+/** Responsividade para telas até 1366px (Notebooks/telas HD+) */
+@media(min-width: 1025px) and (max-width: 1366px) {
+  .btn-input {
+    font-size: 1.25rem;
+  }
+}
+
+/** Responsividade para telas até 1920px (Notebooks/telas FHD) */
+@media(min-width: 1367px) and (max-width: 1920px) {
+  .btn-input {
+    font-size: 1.5rem;
+  }
+}
+
+/** Responsividade para telas maiores que 1920px (Notebooks/telas FHD+) */
+@media(min-width: 1921px) {
+  .btn-input {
+    font-size: 1.5rem;
   }
 }
 </style>
